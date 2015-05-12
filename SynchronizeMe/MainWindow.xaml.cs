ba2c8ae0abcd45cd8fd2f6c56e70f1a9e0e72574
Iuliia Ashomok <bieliaievays@gmail.com>
2015-05-12 15:18:24 +0300

    First commit

---
+++ SynchronizeMe/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SynchronizeMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBrowseSource_Click(object sender, RoutedEventArgs e)
+        {
+            getFolder(TextBoxSource, ref sourceFolderPath);
+        }
+
+        private void ButtonBrowseBackup_Click(object sender, RoutedEventArgs e)
+        {
+            getFolder(TextBoxBackup, ref backupFolderPath);
+        }
+
+        private void ButtonSynchronize_Click(object sender, RoutedEventArgs e)
+        {
+            if (sourceFolderPath != backupFolderPath && sourceFolderPath.Length != 0 && backupFolderPath.Length !=0)
+            { 
+                Synchronizator.Run(sourceFolderPath, backupFolderPath);
+            }
+            else if (sourceFolderPath == backupFolderPath && sourceFolderPath.Length != 0 && backupFolderPath.Length != 0)
+            {
+                MessageBox.Show("Source path and backup path can not be the same.", "", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (sourceFolderPath.Length == 0)
+            {
+                MessageBox.Show("Source path is empty string", "", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (backupFolderPath.Length == 0)
+            {
+                MessageBox.Show("Backup path is empty string", "", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void getFolder(TextBox textBox, ref string selectedPath)
+        {
+            // Create FolderBrowserDialog
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                // Open folder
+                string folderPath = dialog.SelectedPath;
+                textBox.Text = folderPath;
+                selectedPath = folderPath;
+            }
+        }
+
+        private string sourceFolderPath = String.Empty;
+        private string backupFolderPath = String.Empty;
+    }
+}
---
+++ SynchronizeMe/Synchronizator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Files;
+using System.Windows;
+
+namespace SynchronizeMe
+{
+    class Synchronizator
+    {
+        internal static void Run(string sourseFolderPath, string backupFolderPath)
+        {
+            try
+            {
+                FileSyncProvider sourceReplica = new FileSyncProvider(sourseFolderPath);
+
+                FileSyncProvider backupReplica = new FileSyncProvider(backupFolderPath);
+
+                //Initialize the agent which actually performs the
+                //synchronization.
+                SyncOrchestrator agent = new SyncOrchestrator();
+
+                //assign the source replica as the Local Provider and the
+                //backup replica as the Remote provider so that the agent
+                //knows which is the source and which one is the backup.
+                agent.LocalProvider = sourceReplica;
+                agent.RemoteProvider = backupReplica;
+
+                agent.Direction = SyncDirectionOrder.Upload;
+
+                //make a call to the Synchronize method for starting the
+                //synchronization process.
+                agent.Synchronize();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.StackTrace + e.Message, "", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
